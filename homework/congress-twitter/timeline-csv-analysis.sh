d7e9244194f7db2c9bf2e3ae08927d70ef9c78e9
Igor Os <igoros777@gmail.com>
2018-10-29 01:55:28 -0400

    Update timeline-csv-analysis.sh

--- homework/congress-twitter/timeline-csv-analysis.sh
+++ homework/congress-twitter/timeline-csv-analysis.sh
@@ -6,20 +6,19 @@ workdir="/root/data-hold"
 mkdir -p "${workdir}"
 echo "Fetching tweets for ${username} into ${workdir}/${username}"
 file="${workdir}/${username}"
-
 t timeline -n ${postcount} --csv ${username} > ${file}
 count=$(csvfix order -f 1 ${file} | wc -l)
 lastdate=$(csvfix order -fn 'Posted at' ${file} | tail -n 1)
 echo "Analyzing $count tweets by ${1} since ${lastdate}"
 
 echo "Top 10 hashtags by ${username}"
-cat ${file} | csvfix order -fn 'Text' | grep -oE '#[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | grep -vE "$(echo ${excludelist} | sed -r 's/[,; ]/\|/g')" | sort | uniq -c | sort -rn | head -n 10
+cat ${file} | csvfix order -fn 'Text' | grep -oP '#[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | grep -vE "$(echo ${excludelist} | sed -r 's/[,; ]/\|/g')" | sort | uniq -c | sort -rn | head -n 10
 
 echo "Top 10 retweeted users by ${username}"
-cat ${file} | csvfix order -fn 'Text' |  grep -oE 'RT @[A-z0-9_]+' | grep -oE '@[A-z0-9_]+' | sort | uniq -c | sort -rn | head -n 10
+cat ${file} | csvfix order -fn 'Text' |  grep -oP 'RT @[A-z0-9_]+' | grep -oP '@[A-z0-9_]+' | sort | uniq -c | sort -rn | head -n 10
 
 echo "Top 10 mentioned users (not including retweets) by ${username}"
-cat ${file} | csvfix order -fn 'Text' | grep -v "RT" | grep -oE '@[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -rn | head -n 10
+cat ${file} | csvfix order -fn 'Text' | grep -v "RT" | grep -oP '@[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -rn | head -n 10
 
 echo "Top tweeted 10 words with 5+ letters by ${username}"
-cat ${file} | csvfix order -fn 'Text' | grep -oE '[[:alpha:]]{5,}' | sort | uniq -c | sort -rn | head -n 10
+cat ${file} | csvfix order -fn 'Text' | grep -oP '[[:alpha:]]{5,}' | sort | uniq -c | sort -rn | head -n 10
