5ead7ab572c8cd130553f4a53098b647256b32d2
Igor Os <igoros777@gmail.com>
2018-10-29 01:48:10 -0400

    Update timeline-csv-analysis.sh

--- homework/congress-twitter/timeline-csv-analysis.sh
+++ homework/congress-twitter/timeline-csv-analysis.sh
@@ -19,7 +19,7 @@ echo "Top 10 retweeted users by ${username}"
 cat ${file} | csvfix order -fn 'Text' |  grep -oE 'RT @[A-z0-9_]+' | grep -oE '@[A-z0-9_]+' | sort | uniq -c | sort -rn | head -n 10
 
 echo "Top 10 mentioned users (not including retweets) by ${username}"
- cat ${file} | csvfix order -fn 'Text' | grep -v "RT" | grep -oE '@[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -rn | head -n 10
+cat ${file} | csvfix order -fn 'Text' | grep -v "RT" | grep -oE '@[A-z0-9_]+' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -rn | head -n 10
 
 echo "Top tweeted 10 words with 5+ letters by ${username}"
 cat ${file} | csvfix order -fn 'Text' | grep -oE '[[:alpha:]]{5,}' | sort | uniq -c | sort -rn | head -n 10
